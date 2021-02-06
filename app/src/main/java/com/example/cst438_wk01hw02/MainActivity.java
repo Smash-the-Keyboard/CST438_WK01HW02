acebc8b5d53a4e9950e28e0f42c54fa900513992
Christian Sumares <christiansumares@gmail.com>
2021-02-05 21:17:07 -0800

    Login prompt

--- app/src/main/java/com/example/cst438_wk01hw02/MainActivity.java
+++ app/src/main/java/com/example/cst438_wk01hw02/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.cst438_wk01hw02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Button to Log In
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get credentials
+                String username = ((EditText)findViewById(R.id.username)).getText().toString();
+                String password = ((EditText)findViewById(R.id.password)).getText().toString();
+                // Validate credentials
+                if (username.equals("din_djarin") && password.equals("baby_yoda_ftw")) {
+                    // Login successful
+                    finish();
+                }
+                else {
+                    // Login failed
+                    ((TextView)findViewById(R.id.incorrect_password_message)).setText("Incorrect Password");
+                }
+            }
+        });
     }
 }
