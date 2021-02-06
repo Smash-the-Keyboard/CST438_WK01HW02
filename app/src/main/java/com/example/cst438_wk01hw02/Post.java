3b17cf79ad056f407757d08b66e8bfee83703c09
Christian Sumares <christiansumares@gmail.com>
2021-02-05 19:33:02 -0800

    Retrofit & Post object

---
+++ app/src/main/java/com/example/cst438_wk01hw02/Post.java
@@ -0,0 +1,28 @@
+package com.example.cst438_wk01hw02;
+
+import com.google.gson.annotations.SerializedName;
+
+class Post {
+    private int userId;
+    private int id;
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
