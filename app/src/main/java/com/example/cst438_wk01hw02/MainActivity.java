a2f679e31109b96733d9b192a44a6a3e37427eb2
Christian Sumares <christiansumares@gmail.com>
2021-02-05 22:57:45 -0800

    Incorrect username & password messages

--- app/src/main/java/com/example/cst438_wk01hw02/MainActivity.java
+++ app/src/main/java/com/example/cst438_wk01hw02/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cst438_wk01hw02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +13,41 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String MAIN_ACTIVITY = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextUsername = findViewById(R.id.username);
+        EditText editTextPassword = findViewById(R.id.password);
+
+        Log.d(MAIN_ACTIVITY, "Started MainActivity");
+
+        // Reset incorrect username message
+        editTextUsername.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    Log.d(MAIN_ACTIVITY, "Clicked on username field!");
+                    editTextUsername.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    ((TextView)findViewById(R.id.incorrect_username_message)).setText("");
+                }
+            }
+        });
+
+        // Reset incorrect password message
+        editTextPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    Log.d(MAIN_ACTIVITY, "Clicked on password field!");
+                    editTextPassword.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    ((TextView)findViewById(R.id.incorrect_password_message)).setText("");
+                }
+            }
+        });
 
         // Button to Log In
         Button loginButton = findViewById(R.id.login_button);
@@ -23,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Get credentials
-                String username = ((EditText)findViewById(R.id.username)).getText().toString();
-                String password = ((EditText)findViewById(R.id.password)).getText().toString();
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
                 // Validate credentials
                 if (username.equals("din_djarin") && password.equals("baby_yoda_ftw")) {
                     // Login successful
@@ -33,8 +65,14 @@ public class MainActivity extends AppCompatActivity {
                     intent.putExtra("username", username);
                     startActivity(intent);
                 }
-                else {
-                    // Login failed
+                if (!username.equals("din_djarin")) {
+                    // Login failed - incorrect username
+                    editTextUsername.setBackgroundColor(Color.parseColor("#FCD1D1"));
+                    ((TextView)findViewById(R.id.incorrect_username_message)).setText("Incorrect Username");
+                }
+                if (!password.equals("baby_yoda_ftw")) {
+                    // Login failed - incorrect password
+                    editTextPassword.setBackgroundColor(Color.parseColor("#FCD1D1"));
                     ((TextView)findViewById(R.id.incorrect_password_message)).setText("Incorrect Password");
                 }
             }
