c31235668acc93e73dd1025de2eb857e36ee72b6
Christian Sumares <christiansumares@gmail.com>
2021-02-05 23:42:09 -0800

    Comments

--- app/src/main/java/com/example/cst438_wk01hw02/ListPostsActivity.java
+++ app/src/main/java/com/example/cst438_wk01hw02/ListPostsActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,15 +17,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ListPostsActivity extends AppCompatActivity {
 
-    public static final String ACTIVITY_LABEL = "LIST_POSTS_ACTIVITY_COM_EXAMPLE_CST438_WK01HW02";
-
     public static Intent getIntent(Context context) {
         Intent intent = new Intent(context, ListPostsActivity.class);
-        intent.putExtra("activity_name", ".ListPostsActivity");
+        intent.putExtra("activity_name", ".ListPostsActivity"); // Used in unit testing
         return intent;
     }
 
-    private TextView textViewResult, textViewWelcome;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,31 +32,36 @@ public class ListPostsActivity extends AppCompatActivity {
 
         textViewResult = findViewById(R.id.text_view_result);
 
-        textViewWelcome = findViewById(R.id.welcome_message);
-
+        // Generate welcome message
+        TextView textViewWelcome = findViewById(R.id.welcome_message);
         String username = getIntent().getStringExtra("username");
         int userId =  getIntent().getIntExtra("user_id", 1);
-
         textViewWelcome.setText("Welcome, " + username + " (user id " + userId + ")");
 
+        // Create Retrofit instance
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        // Create API instance
         JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
+        // Call getPosts method
         Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
 
+        // Execute call in a separate thread w/callback
         call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                // Display error message
                 if (!response.isSuccessful()) {
                     textViewResult.setText("Code: " + response.code());
                     return;
                 }
                 List<Post> posts = response.body();
                 for (Post post : posts) {
+                    // Only show posts with the same user Id as the user currently logged in
                     if (post.getUserId() == userId) {
                         String content = "";
                         content += "ID: " + post.getId() + "\n";
@@ -70,6 +72,7 @@ public class ListPostsActivity extends AppCompatActivity {
                     }
                 }
             }
+            // Display error message
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
